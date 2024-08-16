11325012a0eb2ebfb58373a79099a8cdd336f0b1
anjali3soni <anjali@gkmit.co>
2024-08-16 17:07:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,81 +1,91 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.61.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = "us-east-2"
+  region = "ap-south-1"
 }
-
-resource "aws_vpc" "my-vpc" {
-  cidr_block = "10.0.0.0/16"
-
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "jenkins-vpc"
+  cidr = "10.0.0.0/16"
+  azs             = ["ap-south-1a"]
+  public_subnets  = ["10.0.101.0/24"]
+  enable_vpn_gateway = true
   tags = {
-    Name = "my-vpc"
+    Terraform = "true"
   }
 }
 
-resource "aws_subnet" "private-sub" {
-  vpc_id     = aws_vpc.my-vpc.id
-  cidr_block = "10.0.1.0/24"
-
+resource "aws_instance" "public_instance" {
+  ami                       = "ami-0ad21ae1d0696ad58"
+  instance_type             = "t2.micro"
+  subnet_id                 = module.vpc.public_subnets[0]
+  associate_public_ip_address = true
+  key_name                  = "terraform"
+  vpc_security_group_ids    = [aws_security_group.public_sg.id]
   tags = {
-    Name = "private-sub"
+    Name = "Public_Instance"
   }
 }
-
-resource "aws_subnet" "public-sub" {
-  vpc_id     = aws_vpc.my-vpc.id
-  cidr_block = "10.0.2.0/24"
-
+resource "aws_security_group" "public_sg" {
+  name        = "public_sg"
+  description = "Allow inbound SSH and HTTP traffic"
+  vpc_id = module.vpc.vpc_id
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
   tags = {
-    Name = "public-sub"
+    Name = "public_sg"
   }
 }
 
-resource "aws_internet_gateway" "my-igw" {
-  vpc_id = aws_vpc.my-vpc.id
-  tags = {
-    Name = "my-igw"
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "jenkins34234-s3-bucket-for-index-html-testing-3931234567"
+}
+resource "aws_s3_bucket_public_access_block" "publicallow" {
+  bucket = aws_s3_bucket.website_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+  depends_on = [aws_s3_bucket_public_access_block.publicallow]
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "PublicReadGetObject"
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.website_bucket.arn}/*"
+      },
+    ]
+  })
+}
+resource "aws_s3_object" "index" {
+  bucket        = aws_s3_bucket.website_bucket.bucket
+  key           = "index.html"
+  source        = "./index.html"
+  content_type  = "text/html"
+  etag          = "${md5(file("./index.html"))}"
+}
+resource "aws_s3_bucket_website_configuration" "web-host-config" {
+  bucket = aws_s3_bucket.website_bucket.id
+  index_document {
+    suffix = "index.html"
   }
 }
-
-resource "aws_route_table" "my-route" {
-  vpc_id = aws_vpc.my-vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my-igw.id
-  }
+output "s3_url" {
+  description = "s3 obj url"
+  value       = aws_s3_bucket_website_configuration.web-host-config.website_endpoint
 }
-
-resource "aws_route_table_association" "pub-sub" {
-  route_table_id = aws_route_table.my-route.id
-  subnet_id      = aws_subnet.public-sub.id
-}
-resource "aws_eip" "my-eip" {
-  domain = "vpc"
-}
-
-resource "aws_nat_gateway" "my-ngw" {
-  subnet_id = aws_subnet.public-sub.id
-  allocation_id = aws_eip.my-eip.id
-  tags = {
-    Name = "my-ngw"
-  }
-  depends_on = [ aws_internet_gateway.my-igw ]
-}
-
-resource "aws_instance" "my-inst" {
-  ami = "ami-0862be96e41dcbf74"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.public-sub.id
-  tags = {
-    Name = "my-inst"
-  }
-}
-

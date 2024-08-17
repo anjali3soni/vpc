ed92f0eaf1af451bc348246348553fffcda1242c
anjali3soni <anjali@gkmit.co>
2024-08-17 13:15:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   name = "jenkins-vpc"
   cidr = "10.0.0.0/16"
-  azs             = ["ap-south-1a"]
+  azs             = ["us-east-1a"]
   public_subnets  = ["10.0.101.0/24"]
   enable_vpn_gateway = true
   tags = {
@@ -13,12 +13,17 @@ module "vpc" {
   }
 }
 
+resource "aws_key_pair" "jk-key" {
+  key_name   = "jk-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+}
+
 resource "aws_instance" "public_instance" {
-  ami                       = "ami-0ad21ae1d0696ad58"
+  ami                       = "ami-04a81a99f5ec58529"
   instance_type             = "t2.micro"
   subnet_id                 = module.vpc.public_subnets[0]
   associate_public_ip_address = true
-  key_name                  = "jkey"
+  key_name                  = aws_key_pair.jk-key.key_name
   vpc_security_group_ids    = [aws_security_group.public_sg.id]
   tags = {
     Name = "Public_Instance"
@@ -47,7 +52,7 @@ resource "aws_security_group" "public_sg" {
 
 
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = "jenkins34234-s3-bucket-for-index-html-testing-3931234567"
+  bucket = "jkt1-1234"
 }
 resource "aws_s3_bucket_public_access_block" "publicallow" {
   bucket = aws_s3_bucket.website_bucket.id

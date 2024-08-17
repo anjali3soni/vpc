fb8f78e52eaf153bbf6fa5956ecbac807feba457
anjali3soni <anjali@gkmit.co>
2024-08-17 14:10:32 +0530

    final commit

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
   name = "jenkins-vpc"
   cidr = "10.0.0.0/16"
-  azs             = ["us-east-1a"]
+  azs             = ["us-east-2a"]
   public_subnets  = ["10.0.101.0/24"]
   enable_vpn_gateway = true
   tags = {
